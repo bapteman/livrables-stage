549250cdd634938fe6a9071f2776427f4f17859b
Baptiste Seneclauze <baptiste@mbp-de-laura.home>
2022-06-30 14:19:16 +0200

    second commit

--- popup.js
+++ popup.js
@@ -3,53 +3,70 @@ let img = chrome.runtime.getURL("player_image.png");
 //import {tester} from "test.js";
 //  parse = require('test.js');
 
-function test_theme() {
+/* function test_theme() {
   chrome.storage.sync.get("tst", ({ tst }) => {
-    document.head.insertAdjacentHTML("beforeend", `<style> ${tst} </style>`);
-
-    /* var theme = document.createElement("style")
+    chrome.storage.sync.get("couleur", ({ couleur }) => {
+      document.head.insertAdjacentHTML(
+        "beforeend",
+        `<style> ${tst} </style> <style> .audion-360-print-audio-player-btn{background-color : ${couleur}} </style> <style> .audion-360-print-audio-player-tag{background-color : ${couleur}} </style>`
+      );
+       document.head.insertAdjacentHTML(
+        "beforeend",
+        `<style> .audion-360-print-audio-player-btn{background-color : ${couleur}} </style>`
+      );
+      document.head.insertAdjacentHTML(
+        "beforeend",
+        `<style> .audion-360-print-audio-player-tag{background-color : ${couleur}} </style>`
+      ); 
+       var theme = document.createElement("style")
         theme.href=tst;
-      document.head.appendChild(theme);  */
+      document.head.appendChild(theme);  
+    });
   });
-}
+} */
 // Add a button to the page for each supplied color
 function constructButtons() {
   //test button
   let button = document.createElement("button");
   button.textContent = "test";
   button.addEventListener("click", async () => {
+    //alert("bouton cliqué");
     setValues();
+    //alert("values set");
     const [tab] = await chrome.tabs.query({
       active: true,
       currentWindow: true,
     });
-    //page.appendChild(img);
-    //alert(document.getElementById("theme").value);
-    //function: test_theme,
-    //document.querySelector(".audion-360-print-audio-player-tag").textContent = "Ecouter cet article"
     loadscriptplayer(tab);
   });
-
   page.appendChild(button);
-  /*let button2 = document.createElement("button");
-      button2.textContent= "theme";
-      button2.addEventListener("click", async () => { 
-        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-        chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          function : test_theme,
-      }) 
-   })*/
-  //page.appendChild(button2);
+
+  /* let button2 = document.createElement("button");
+  button2.textContent = "theme";
+  button2.addEventListener("click", async () => {
+    setValues();
+
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      //function: test_theme(couleur),
+    });
+  });
+  page.appendChild(button2); */
 }
 
 function loadscriptplayer(tab) {
+  //alert("loadscript player ");
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ["script-player.js"],
   });
-  document.querySelector(".audion-360-print-audio-player-tag").textContent =
-    "Ecouter cet article";
+  //alert("fonction finie");
+  //document.querySelector(".audion-360-print-audio-player-tag").textContent =
+  // "Ecouter cet article";
 }
 
 function setValues() {
@@ -60,8 +77,11 @@ function setValues() {
     ancre: document.querySelector("#ancre").value,
     position: document.querySelector("#position").value,
     tst: document.getElementById("theme").value,
-    titre: "Resources for developers",
-    // titre: document.querySelector("h1").value
+    titre: document.querySelector("#titre").value,
+    couleur: document.querySelector("#couleur").value,
+    marge_top: document.querySelector("#marge-top").value,
+    marge_bottom: document.querySelector("#marge-bottom").value,
+    sombre: document.querySelector("#theme_sombre").value,
   });
 }
 
--- script-player.js
+++ script-player.js
@@ -1,98 +1,137 @@
-(function () {
-  chrome.storage.sync.get("position", ({ position }) => {
-    chrome.storage.sync.get("ancre", ({ ancre }) => {
-      chrome.storage.sync.get("titre", ({ titre }) => {
-        alert(position + ancre + titre);
-        var sessionId = null;
-        var sessionIdEventDebug = undefined;
-        var collectionId = "uELp0anTnrqw";
-        var canonicalUrl = getCanonical();
-        var referer = window.location.origin;
-        var containers = {
-          thin: { selector: `${ancre}`, position: `${position}` },
-          //float: { selector: "main", position: "beforeend" },
-        };
-        var mainContainer =
-          containers.thin || containers.sticky || containers.float;
-        function getCanonical() {
-          var canonical = document.querySelector("[rel=canonical]");
-          return (
-            (canonical && canonical.href) ||
-            window.location.origin + window.location.pathname
-          );
-        }
+chrome.storage.sync.get("position", ({ position }) => {
+  chrome.storage.sync.get("ancre", ({ ancre }) => {
+    chrome.storage.sync.get("titre", ({ titre }) => {
+      chrome.storage.sync.get("couleur", ({ couleur }) => {
+        chrome.storage.sync.get("marge_top", ({ marge_top }) => {
+          chrome.storage.sync.get("marge_bottom", ({ marge_bottom }) => {
+            chrome.storage.sync.get("sombre", ({ sombre }) => {
+              chrome.storage.sync.get("tst", ({ tst }) => {
+                alert(position + ancre + titre);
+                var sessionId = null;
+                var sessionIdEventDebug = undefined;
+                var collectionId = "uELp0anTnrqw";
+                var canonicalUrl = getCanonical();
+                var referer = window.location.origin;
+                var containers = {
+                  thin: { selector: `${ancre}`, position: `${position}` },
+                  //float: { selector: "main", position: "beforeend" },
+                };
+                var mainContainer =
+                  containers.thin || containers.sticky || containers.float;
+                function getCanonical() {
+                  var canonical = document.querySelector("[rel=canonical]");
+                  return (
+                    (canonical && canonical.href) ||
+                    window.location.origin + window.location.pathname
+                  );
+                }
 
-        function setupContainer() {
-          var container = document.createElement("div");
-          container.id = "audion-360-print-audio-player";
-          container.dataset.id = collectionId;
-          container.dataset.sessionId = sessionId;
-          container.dataset.canonicalUrl = canonicalUrl;
-          container.dataset.referer = referer;
-          container.dataset.players = JSON.stringify({
-            thin: "audion-360-print-audio-player-root-thin",
-            //float: "audion-360-print-audio-player-root-float",
+                function setupContainer() {
+                  var container = document.createElement("div");
+                  container.id = "audion-360-print-audio-player";
+                  container.dataset.id = collectionId;
+                  container.dataset.sessionId = sessionId;
+                  container.dataset.canonicalUrl = canonicalUrl;
+                  container.dataset.referer = referer;
+                  container.dataset.players = JSON.stringify({
+                    thin: "audion-360-print-audio-player-root-thin",
+                    //float: "audion-360-print-audio-player-root-float",
+                  });
+                  container.dataset.title = `${titre}`;
+                  container.dataset.lang = "fr-FR";
+                  container.dataset.dictionary =
+                    '{"listen_to_this_article":"Ecouter cet article","ad":"Annonce","your_content_after_this_ad":"Votre contenu après cette annonce","powered_by":"Powered by","wait":"/wait.fr.mp3"}';
+                  container.style.display = "none";
+                  var mainSelector = document.querySelector(
+                    mainContainer.selector
+                  );
+                  mainSelector.insertAdjacentElement(
+                    mainContainer.position,
+                    container
+                  );
+                }
+                function createPlayer() {
+                  var playerUrl = "https://dev-player.360.audion.fm/v3/latest";
+                  var container = document.querySelector(
+                    mainContainer.selector
+                  );
+                  var link = document.createElement("link");
+                  link.rel = "stylesheet";
+                  link.href = playerUrl + "/index.css";
+                  container.append(link);
+
+                  var parser = new DOMParser();
+                  var doc = parser.parseFromString(
+                    getPlayerHTML(
+                      document.querySelector(`${titre}`).textContent
+                    ),
+                    "text/html"
+                  );
+                  var setupBody = doc.querySelector(
+                    "#audion-360-print-audio-player-setup"
+                  );
+                  if (setupBody && container) {
+                    container.append(setupBody);
+                    // var script = document.createElement("script");
+                    // script.src = "./index.js";
+                    // container.append(script);
+                  }
+                  var playerThinBody = doc.querySelector(
+                    "#audion-360-print-audio-player-body-thin"
+                  );
+                  var playerThinSelector = document.querySelector(
+                    containers.thin && containers.thin.selector
+                  );
+                  if (playerThinBody && playerThinSelector) {
+                    playerThinSelector.insertAdjacentElement(
+                      containers.thin.position,
+                      playerThinBody
+                    );
+                  }
+                  const btnPlays = Array.from(
+                    document.getElementsByClassName(
+                      "audion-360-print-audio-player-btn"
+                    )
+                  );
+                  const playerAudio = document.getElementById(
+                    "audion-360-print-audio-player-audio"
+                  );
+                  btnPlays.forEach((btn) =>
+                    btn.addEventListener("click", () => {
+                      playerAudio.play();
+                      playerAudio.autoplay = true;
+                    })
+                  );
+                }
+                function setupPlayer() {
+                  setupContainer();
+                  createPlayer();
+                }
+                setupPlayer();
+                if (couleur) {
+                  setupCouleur(couleur);
+                }
+                if (marge_top) {
+                  setupMargetop(marge_top);
+                }
+                if (marge_bottom) {
+                  setupMargebottom(marge_bottom);
+                }
+                if (tst) {
+                  setupTheme(tst);
+                }
+                alert("sombre");
+                if (sombre) {
+                  setupSombre();
+                }
+              });
+            });
           });
-          container.dataset.title = `${titre}`;
-          container.dataset.lang = "fr-FR";
-          container.dataset.dictionary =
-            '{"listen_to_this_article":"Ecouter cet article","ad":"Annonce","your_content_after_this_ad":"Votre contenu après cette annonce","powered_by":"Powered by","wait":"/wait.fr.mp3"}';
-          container.style.display = "none";
-          var mainSelector = document.querySelector(mainContainer.selector);
-          mainSelector.insertAdjacentElement(mainContainer.position, container);
-        }
-        function createPlayer() {
-          var playerUrl = "https://dev-player.360.audion.fm/v3/latest";
-          var container = document.querySelector(mainContainer.selector);
-          var link = document.createElement("link");
-          link.rel = "stylesheet";
-          link.href = playerUrl + "/index.css";
-          container.append(link);
-
-          var parser = new DOMParser();
-          var doc = parser.parseFromString(getPlayerHTML(titre), "text/html");
-          var setupBody = doc.querySelector(
-            "#audion-360-print-audio-player-setup"
-          );
-          if (setupBody && container) {
-            container.append(setupBody);
-            // var script = document.createElement("script");
-            // script.src = "./index.js";
-            // container.append(script);
-          }
-          var playerThinBody = doc.querySelector(
-            "#audion-360-print-audio-player-body-thin"
-          );
-          var playerThinSelector = document.querySelector(
-            containers.thin && containers.thin.selector
-          );
-          if (playerThinBody && playerThinSelector) {
-            playerThinSelector.insertAdjacentElement(
-              containers.thin.position,
-              playerThinBody
-            );
-          }
-          const btnPlays = Array.from(
-            document.getElementsByClassName('audion-360-print-audio-player-btn')
-          );
-          const playerAudio = document.getElementById(
-            'audion-360-print-audio-player-audio'
-          );
-          btnPlays.forEach((btn) => btn.addEventListener('click', () => {
-            playerAudio.play();
-            playerAudio.autoplay = true;
-          }));
-        }
-        function setupPlayer() {
-          setupContainer();
-          createPlayer();
-        }
-        setupPlayer();
+        });
       });
     });
   });
-})();
-
+});
 
 function getPlayerHTML(title) {
   return `
@@ -156,4 +195,37 @@ function getPlayerHTML(title) {
     <div id="audion-360-print-audio-player-root-thin"></div>
 </div>
 </div>
-`}
+`;
+}
+
+function setupCouleur(couleur) {
+  document.head.insertAdjacentHTML(
+    "beforeend",
+    `<style> .audion-360-print-audio-player-btn{background-color : ${couleur} !important} </style> <style> .audion-360-print-audio-player-tag{background-color : ${couleur} !important} </style>`
+  );
+}
+
+function setupMargetop(marge_top) {
+  document.head.insertAdjacentHTML(
+    "beforeend",
+    `<style> #audion-360-print-audio-player-body-thin{margin-top :${marge_top};}`
+  );
+}
+
+function setupMargebottom(marge_bottom) {
+  document.head.insertAdjacentHTML(
+    "beforeend",
+    `<style> #audion-360-print-audio-player-body-thin{margin-bottom : ${marge_bottom}}`
+  );
+}
+
+function setupTheme(tst) {
+  document.head.insertAdjacentHTML("beforeend", `<style> ${tst} </style> `);
+}
+
+function setupSombre() {
+  document.head.insertAdjacentHTML(
+    "beforeend",
+    `<style>.audion-360-print-audio-player-titleText{color : white !important;}.audion-360-print-audio-player-time{color : white !important;}.audion-360-print-audio-player-poweredBy{color : white !important;}</style>`
+  );
+}
